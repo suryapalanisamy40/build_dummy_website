f8464e5496494f36e06365dacc8580c0fe3c841b
unknown <suryapalanisamy40@gmail.com>
2023-08-26 15:06:13 +0530

    Dummy site

---
+++ public/Js.js
@@ -0,0 +1,9 @@
+let fasolid = document.getElementsByClassName("fa-solid");
+let ul = document.getElementsByClassName("ul")
+// let show = document.getElementsByClassName("show")
+fasolid[0].addEventListener("click", () => {
+    ul[0].classList.toggle("show");
+    
+    console.log("asdf");
+
+})
